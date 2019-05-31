84a4e6f35695378a0ef0d640e68d398312d80a4d
Douglas <douglaaa.s@hotmail.com>
2019-05-30 22:43:15 -0300

    api concluida

--- app/services/recebe_temp.rb
+++ app/services/recebe_temp.rb
@@ -6,7 +6,14 @@ class RecebeTemp
     def buscar(cidade)
         @cidade = cidade
 
-        url = "http://api.openweathermap.org/data/2.5/weather?q=#{@cidade},BR&APPID=47eea82303cf3b327239995184d03037"
+        url = "http://api.openweathermap.org/data/2.5/weather?q=#{@cidade},BR&APPID=47eea82303cf3b327239995184d03037&units=metric"
         retorno = JSON.parse(Net::HTTP.get(URI(url)))
+
+        if retorno["message"]
+            return {message: "cidade não existe"}
+        else    
+            retorno["main"]["temp"]
+        end
+
     end
-end            
+end             
