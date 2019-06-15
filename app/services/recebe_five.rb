65c1b991a390634878734fcbd3c157bdbcd2ac35
Douglas <douglaaa.s@hotmail.com>
2019-06-14 21:49:09 -0300

    vai louco

--- app/services/recebe_five.rb
+++ app/services/recebe_five.rb
@@ -8,12 +8,12 @@ class RecebeFive
         url = "http://api.openweathermap.org/data/2.5/forecast?q=#{@cidade},BR&APPID=47eea82303cf3b327239995184d03037"
         retorno = JSON.parse(Net::HTTP.get(URI(url)))
     
-        teste = [];
+        dados = [];
         response["list"].each do <pipe>hora<pipe>
-            teste << [hora] hora["dt"] >>
+            tedadosste << [hora] hora:["dt"]
         end
 
-        teste;
+        dados;
     end   
 
             
