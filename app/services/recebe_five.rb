e748b1c4ed8f21537afe99cedc7d9bd75b6c9293
Douglas <douglaaa.s@hotmail.com>
2019-06-14 21:37:05 -0300

    array json

--- app/services/recebe_five.rb
+++ app/services/recebe_five.rb
@@ -7,6 +7,12 @@ class RecebeFive
 
         url = "http://api.openweathermap.org/data/2.5/forecast?q=#{@cidade},BR&APPID=47eea82303cf3b327239995184d03037"
         retorno = JSON.parse(Net::HTTP.get(URI(url)))
+    
+    jsonGroup = [
+        -> retorno['main'],
+        -> retorno['dt']
+    ];
+    
     end   
 
             
