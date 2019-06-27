84ca0ff59500dfd68cae87307d287de64fa872da
Douglas <douglaaa.s@hotmail.com>
2019-06-26 22:37:04 -0300

    finalizando segundo serviço

--- app/services/recebe_five.rb
+++ app/services/recebe_five.rb
@@ -9,13 +9,20 @@ class RecebeFive
         retorno = JSON.parse(Net::HTTP.get(URI(url)))
     
         dados = []
-        retorno['list'].each do |hour|
-            dados << {hora: hour[:dt],clima:{temperatura: hour[:main][:temp]}}
-        end      
-        dados;
-    end   
+  
+        retorno["list"].each do |dia|
+            dados << {hora: Time.at(dia["dt"]), 
+            Clima:{Temperatura: dia["main"]["temp"],
+            Descrição: dia["weather"][0]["description"],
+            Pressao: dia["main"]["pressure"],
+            Humidade: dia["main"]["humidity"], 
+            Temperatura_Maxima: dia["main"]["temp_max"], 
+            Temperatura_Minima: dia["main"]["temp_min"], 
+            Velocidade_Vento: dia["wind"]["speed"]}} 
 
-            
+        end
+        dados
+    end        
         
     
 end        
